cd78d12682bd492a1c05da7a6107156846997bd5
Procope <m.giulianelli.m@gmail.com>
2018-05-05 14:47:45 +0200

    Minor

--- lab2/lexical_substitution.py
+++ lab2/lexical_substitution.py
@@ -3,130 +3,139 @@ import pickle
 
 
 def skipgram_scores(embeds, target, alternatives, context, mode='cosine'):
+    '''
+    Scoring functions from:
+    Melamud, Oren, Omer Levy, and Ido Dagan.
+    "A simple word embedding model for lexical substitution."
+    Proceedings of the 1st Workshop on Vector Space Modeling for Natural Language Processing. 2015.
+    '''
+    t = embeds[target]
+    alternatives = np.array([embeds[a] for a in alternatives])
+    c_len = len(context)
+    c = np.array([embeds[w] for w in context])
 
-  t = embeds[target]
-  alternatives = np.array([embeds[a] for a in alternatives])
-  c_len = len(context)
-  c = np.array([embeds[w] for w in context])
+    if mode == 'cosine':
+            scores = alternatives @ t
+            scores = scores.tolist()
 
-  if mode == 'cosine':
-    scores = alternatives @ t
-    scores = scores.tolist()
-
-  elif mode == 'add':
-    scores = [
-        (a @ t + np.sum(c @ a)) / (c_len + 1)
-        for a
-        in alternatives
+    elif mode == 'add':
+            scores = [
+                (a @ t + np.sum(c @ a)) / (c_len + 1)
+            for a
+            in alternatives
     ]
 
-  elif mode == 'baladd':
-    scores = [
-        (c_len * (a @ t) + np.sum(c @ a)) / (2 * c_len)
-        for a
-        in alternatives
-    ]
+    elif mode == 'baladd':
+        scores = [
+                (c_len * (a @ t) + np.sum(c @ a)) / (2 * c_len)
+                for a
+                in alternatives
+        ]
 
-  elif mode == 'mult':
-    scores = [
-        (((u @ v + 1) / 2) * np.prod((c @ a + 1) / 2)) ** (1 / (c_len + 1))
-        for a
-        in alternatives
-    ]
+    elif mode == 'mult':
+        scores = [
+                (((u @ v + 1) / 2) * np.prod((c @ a + 1) / 2)) ** (1 / (c_len + 1))
+                for a
+                in alternatives
+        ]
 
-  elif mode == 'balmult':
-    scores = [
-        ((((u @ v + 1) / 2) ** c_len) * np.prod((c @ a + 1) / 2)) ** (2 * c_len)
-        for a
-        in alternatives
-    ]
+    elif mode == 'balmult':
+        scores = [
+                ((((u @ v + 1) / 2) ** c_len) * np.prod((c @ a + 1) / 2)) ** (2 * c_len)
+                for a
+                in alternatives
+        ]
 
-  elif mode == 'test':
-    return range(len(alternatives))
-  else:
-    raise ValueError('Mode: [add, baladd, mult, balmult, test]')
+    elif mode == 'test':
+        return range(len(alternatives))
+    else:
+        raise ValueError('Mode: [add, baladd, mult, balmult, test]')
 
 
 def kl_div(u, l, m_1, m_2):
-  # u,l are cov matrices
-  # m_1 and m_2 are mean vectors
-  kl = -0.5 * np.trace(np.matmul(u, l)) \
-      + np.matmul(np.matmul(np.transpose(m_1 - m_2), np.linalg.inv(u)), (m_1 - m_2)) - dim \
-      + np.log(np.linalg.det(l) / np.linalg.det(u))
-  return kl
+    # u,l are cov matrices
+    # m_1 and m_2 are mean vectors
+    kl = -0.5 * np.trace(np.matmul(u, l)) \
+            + np.matmul(np.matmul(np.transpose(m_1 - m_2), np.linalg.inv(u)), (m_1 - m_2)) - dim \
+            + np.log(np.linalg.det(l) / np.linalg.det(u))
+    return kl
 
 
 def embedalign_scores(embeds_means, embeds_cov_matrices, target, alternatives, context):
 
-  t_mean = embeds_means[target]
+    t_mean = embeds_means[target]
+    t_cov_matrix = embeds_cov_matrices[:, :, target]
 
-  dim = len(t_mean)
+    dim = len(t_mean)
+    alternatives_len = len(alternatives)
 
-  t_cov_matrix = embeds_cov_matrices[:, :, target]
+    alternatives_means = np.array([embeds_means[a] for a in alternatives])
+    alternatives_cov_matrices = np.array([embeds_cov_matrices[:, :, a] for a in alternatives])
 
-  alternatives_len = len(alternatives)
+    # scoring
+    scores = [
+            kl_div(alternatives_cov_matrices[:, :, a],
+                   t_cov_matrix,
+                   alternatives_means[a],
+                   t_mean)
+            for a
+            in alternatives
+    ]
 
-  alternatives_means = np.array([embeds_means[a] for a in alternatives])
-
-  alternatives_cov_matrices = np.array([embeds_cov_matrices[:, :, a] for a in alternatives])
-
-  # scoring
-  scores = []
-  for a in alternatives:
-    # KL(candidate||target)
-    score = kl_div(alternatives_cov_matrices[:, :, a], t_cov_matrix, alternatives_means[a], t_mean)
-    scores.append(score)
-
-  return scores
+    return scores
 
 
 if __name__ == "__main__":
 
-  with open('w2i-europarl-en.p', 'rb') as f:
-    word2idx = pickle.load(f)
+    with open('w2i-europarl-en.p', 'rb') as f:
+        word2idx = pickle.load(f)
 
-  with open('data/lst/lst.gold.candidates', 'r') as f:
-    lines = map(str.strip, f.readlines())
+    with open('data/lst/lst.gold.candidates', 'r') as f:
+        lines = map(str.strip, f.readlines())
 
-  candidates = {}
 
-  for line in lines:
-    target, rest = line.split('.', maxsplit=1)
-    pos_tag, rest = rest.split('::', maxsplit=1)
-    alternatives = rest.split(';')
-
-    candidates[target] = alternatives
-
-  with open('data/lst/lst_test.preprocessed', 'r') as f:
-    lines = map(str.strip, f.readlines())
-
-  skipped_entries = 0
-  with open('lst.out', 'w') as f_out:
+    candidates = {}
     for line in lines:
-      target, sent_id, target_position, sentence = line.split('\t')
+        target, rest = line.split('.', maxsplit=1)
+        pos_tag, rest = rest.split('::', maxsplit=1)
+        alternatives = rest.split(';')
+        candidates[target] = alternatives
 
-      target_word = target.split('.')[0]
-      sentence = sentence.split()
 
-      try:
-        target_id = word2idx[target_word]
-      except KeyError:
-        skipped_entries += 1
-        continue
-      # sentence_ids = [word2idx[w] for w in sentence if w in word2idx.keys()]
+    with open('data/lst/lst_test.preprocessed', 'r') as f:
+        lines = map(str.strip, f.readlines())
 
-      alternatives = candidates[target_word]
-      alternative_ids = [word2idx[w] for w in alternatives if w in word2idx.keys()]
+    skipped_entries = 0
+    with open('lst.out', 'w') as f_out:
+        for line in lines:
+            target, sent_id, target_position, sentence = line.split('\t')
 
-      scores = skipgram_scores(None, target_id, alternative_ids, sentence, 'test')
+            target_word = target.split('.')[0]
+            sentence = sentence.split()
 
-      print('RANKED\t{} {}'.format(target, sent_id), file=f_out, end='')
+            try:
+                target_id = word2idx[target_word]
+            except KeyError:
+                skipped_entries += 1
+                continue
 
-      words_and_scores = list(zip(alternatives, scores))
-      words_and_scores.sort(key=lambda t: t[1], reverse=True)
+            # sentence_ids = [word2idx[w] for w in sentence if w in word2idx.keys()]
+            alternatives = candidates[target_word]
+            alternative_ids = [word2idx[w] for w in alternatives if w in word2idx.keys()]
 
-      for w, s in words_and_scores:
-        print('\t{} {}'.format(w, s), file=f_out, end='')
-      print(file=f_out)
+            # Score alternatives
+            scores = skipgram_scores(None, target_id, alternative_ids, sentence, 'test')
 
-  print("{} entries have been skipped.".format(skipped_entries))
+            # Print preamble
+            print('RANKED\t{} {}'.format(target, sent_id), file=f_out, end='')
+
+            # Sort alternative by their scores
+            words_and_scores = list(zip(alternatives, scores))
+            words_and_scores.sort(key=lambda t: t[1], reverse=True)
+
+            # Write ranked alternatives and their scores to file
+            for w, s in words_and_scores:
+                print('\t{} {}'.format(w, s), file=f_out, end='')
+            print(file=f_out)
+
+    print("{} entries have been skipped.".format(skipped_entries))
