43eda266952133d0b656f5e2a48f7082818122b7
Procope <m.giulianelli.m@gmail.com>
2018-05-22 20:25:49 +0200

    Make stop-word filtering  optional

--- lab3/skipgram.py
+++ lab3/skipgram.py
@@ -11,6 +11,8 @@ FREQUENCY_THRESHOLD = 1
 EMBED_DIMS = 100
 WINDOW_SIZE = 5
 NEGATIVE_SAMPLING = 5
+NUM_EPOCHS = 30
+STOP_WORDS = False
 
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -21,16 +23,22 @@ print('Folder "{}" will be used to save temporary dictionary and corpus.'.format
 with open('../lab2/data/europarl/training.en', 'r') as corpus_f:
     corpus = [line.strip() for line in corpus_f.readlines()]
 
-with open('../lab2/stop_words_en.txt', 'r') as f_in:
-    stop_words = [line.strip() for line in f_in.readlines()]
+if STOP_WORDS:
+    with open('../lab2/stop_words_en.txt', 'r') as f_in:
+        stop_words = [line.strip() for line in f_in.readlines()]
 
 
 print('Tokenize corpus...')
 start = time.time()
-tokenized_corpus = [[token for token in sentence.split()
+if STOP_WORDS:
+    tokenized_corpus = [[token for token in sentence.split()
                         if token not in stop_words]
                         for sentence in corpus]
-print("Done in {:2} seconds".format(time.time() - start))
+else:
+    tokenized_corpus = [[token for token in sentence.split()]
+                        for sentence in corpus]
+
+print("Done in {} seconds".format(time.time() - start))
 
 
 model = gensim.models.Word2Vec(
@@ -44,6 +52,6 @@ model = gensim.models.Word2Vec(
         workers=4)
 
 
-model.train(tokenized_corpus, total_examples=len(tokenized_corpus), epochs=10)
+model.train(tokenized_corpus, total_examples=len(tokenized_corpus), epochs=NUM_EPOCHS)
 
 model.save('skipgram-{}-w{}-fr{}-ns{}.embs'.format(EMBED_DIMS, WINDOW_SIZE, FREQUENCY_THRESHOLD, NEGATIVE_SAMPLING))
