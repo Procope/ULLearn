05c597ddc7a0e532949fa4c9fe2fc84287359955
Procope <m.giulianelli.m@gmail.com>
2018-05-31 15:21:39 +0200

    Update tasks

--- lab3/senteval_glove.py
+++ lab3/senteval_glove.py
@@ -156,11 +156,8 @@ if __name__ == "__main__":
     # in (https://arxiv.org/abs/1802.05883) we use the following :
     # SICKRelatedness (Sick-R) needs torch cuda to work (even when using logistic regression),
     # but STS14 (semantic textual similarity) is a similar type of semantic task
-    # transfer_tasks = ['CR', 'MR', 'MPQA', 'SUBJ', 'SST2', 'STS16',
-    #                   'SST5', 'TREC', 'MRPC', 'SICKEntailment',
-    #                   'Depth', 'BigramShift', 'Tense', 'SubjNumber']
-
-    transfer_tasks = ['TopConstituents', 'ObjNumber', 'OddManOut', 'CoordinationInversion', 'Length']
+    transfer_tasks = ['CR', 'MR', 'MPQA', 'SUBJ', 'SST2', 'STS16', 'SST5', 'TREC', 'MRPC', 'SICKEntailment',
+                      'Depth', 'BigramShift', 'Tense', 'SubjNumber','TopConstituents', 'Length']
 
     # senteval prints the results and returns a dictionary with the scores
     results = se.eval(transfer_tasks)
